e4219255a2bc7b07a3d6909e2c6a6335fe74ce8f
Diego Córdova <60157586+Diego-CB@users.noreply.github.com>
2024-11-15 18:40:51 -0600

    Update retrain.py

--- models/retrain.py
+++ models/retrain.py
@@ -1,5 +1,5 @@
-from models.train import append_new_record, train_model
-from models.train2 import append_new_record2, train_model2
+from train import append_new_record, train_model
+from train2 import append_new_record2, train_model2
 
 train_model()
 train_model2()
